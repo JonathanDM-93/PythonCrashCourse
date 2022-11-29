e5850de0053b2bb99ba6c824558fa2f22e4a3940
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2022-11-29 00:10:57 -0600

    Try it Yourself pagina 23

---
+++ PythonExercices/TryitYourselft_23.py
@@ -0,0 +1,12 @@
+# TRY IT YOURSELF pag 23 crear variables e imprimirlas
+# Crear variables e imprimirlas
+# 2.1 Mensaje simple
+my_Var = "Hola nuevo proyecto en Python 29 Noviembre 2022"
+print(my_Var)
+
+# 2.2 Mensaje simple
+
+my_Var = "Nuevo mensaje con la misma variable"
+print(my_Var)
+
+
