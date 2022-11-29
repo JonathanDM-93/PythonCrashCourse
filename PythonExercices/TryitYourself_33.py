f632675e8f8a51fb8277bfc825b86209dedef6ed
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2022-11-29 13:35:50 -0600

    Try it Yourself pagina 33

---
+++ PythonExercices/TryitYourself_33.py
@@ -0,0 +1,18 @@
+# TRY IT YOURSELF pag 33 Numbers
+# 2.8 Crear una variable que almacene una operacion aritmetica que devuelva un valor
+mySum = 9 + 1 + 3
+myRest = 11 - 1
+myDiv = 1234 / 10
+
+# Guarde los resultados en una lista
+simpleList = [mySum, myRest, myDiv]
+# Los pase a traves de un for para imprimir el resultado
+for i in simpleList:
+    print("Resultado: " + str(i))
+
+# 2.9 Mi número favorito
+# Aquí posiblemente sea una buena práctica declarar el tipo de variable que estoy utilizando
+ChoseOne: int = 25
+phrase: str = "Aquí mi número favorito es: "
+print(phrase + str(ChoseOne))
+
