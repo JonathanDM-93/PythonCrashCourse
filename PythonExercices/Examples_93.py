e7f3d913744e8b6aa00917ca452e02959ebfe0e3
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2022-12-19 18:06:59 -0600

    Ejemplos Try it Yourself pag 93 x2

--- PythonExercices/Examples_93.py
+++ PythonExercices/Examples_93.py
@@ -17,3 +17,28 @@ for item in UsersList:
 
 print('\n[*]\n')
 
+# En el siguiente ejemplo crearemos dos listas que contengan nombres de usuarios registrados
+# y otra lista de seran nombres de usuarios nuevos compararemos una con otra y mandaremos un
+# MENSAJE para comentar si el nombre ya se encuentra registrado
+
+
+UserRegi: list = ['Jonathan', 'Peter', 'Maria', 'Karen', 'Nadia']
+NewUser: list = ['Maria', 'Nadia', 'Kevin']
+
+for item in NewUser:
+    if item in UserRegi:
+        print('Elige un nuevo nombre')
+    else:
+        print('El nombre que elegiste no se repite')
+
+# Lo que se quiere dar a entender en el ejemplo es que se puede comparar dos listas y si dentro
+# de ellas se encuentra un elemento idéntico
+# en este caso lo que se imprime es:
+
+# Elige un nuevo nombre
+# Elige un nuevo nombre
+# El nombre que elegiste no se repite
+
+# Porque dos de los nombres se repiten y el último no.
+
+
