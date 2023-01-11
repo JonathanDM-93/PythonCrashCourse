b2458a5737efd7e00f6ba798aa0473c149322872
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-01-11 17:28:43 -0600

    Ejemplo 6-10 Favorite numbers

--- PythonExercices/Diccionarios/TryitYourSelf_114.py
+++ PythonExercices/Diccionarios/TryitYourSelf_114.py
@@ -59,5 +59,27 @@ namePets: list = [Draks, Luke]
 for name in namePets:
     print(name)
 
+print('\n[*]\n')
+
 # EJEMPLO 6-9 Favorite Places
+# 1.- Crea un diccionario llamado favorite_places, piensa en tres lugares para que sirvan como KEYS
+# y guarda para cada lugar una persona.
+# 2.- recorre cada diccionario e imprime el nombre de la persona con su sitio favorito.
+
+favorite_places: dict = {
+    'Cancún': 'Jonathan',
+    'Mazatlán': 'Gina',
+    'Acapulco': 'Enrique',
+    'Teotihuacan': 'Kevin',
+}
+
+for places, names in favorite_places.items():
+    print(names + ' ama visitar ' + places)
+# >>
+# Jonathan ama visitar Cancún
+# Gina ama visitar Mazatlán
+# Enrique ama visitar Acapulco
+# Kevin ama visitar Teotihuacan
+
+# EJEMPLO 6-10 Favorite numbers
 
