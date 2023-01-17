ee03289ca2173b099632daca5f2212d4e82eaf68
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-01-16 20:19:52 -0600

    Se inicio con el bucle while

---
+++ PythonExercices/Bucle_WHILE/counting.py
@@ -0,0 +1,7 @@
+# El bucle while únicamente se ejecutará si alguna condición es verdadera.
+# El siguiente es un bloque while básico
+
+contador: int = 1
+while contador <= 5:
+    print(contador)
+    contador += 1 # Colocar un contador que vaya aumentando
