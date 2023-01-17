470f754514be20f776490698d8df67c2be58bba1
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-01-16 20:07:50 -0600

    Se agregaron ejemplos alÃ Try it Yourself pag 121

--- PythonExercices/Bucle_WHILE/TryitYourself_121.py
+++ PythonExercices/Bucle_WHILE/TryitYourself_121.py
@@ -10,3 +10,28 @@ for modelos in Active_cars['cars']:
         print('Si tenemos disponible:')
     else:
         print('No tenemos el auto que buscas. ¿Quieres que te muestre otro auto?')
+
+print('\n[*]\n')
+
+# EJERCICIO 7-2 Restaurant Seating
+# Crea un programa en donde se pregunte al usuario cuantas personas estaran en una cena
+# si superan a más de ocho devolver un mensaje que deberan esperan una mesa pero si son
+# menos de ocho decir que la mesa está disponible
+
+print('\t Bienvenidos!')
+dinner_people: int = int(input('- ¿Cuantas personas vienen a cenar? '))
+if dinner_people >= 8:
+    print('- Debera esperar un poco para su mesa.')
+else:
+    print('\n -Su mesa esta disponible. Pasen!')
+
+# EJERCICIO 7-3 Multiples of ten
+# Reporta si el número que ingresa un usuario es multiplo de 10.
+# Aquí, use la analogía del número par solo que si al multiplicar por si mismo
+# y al dividir entre 10 debe dar 0.
+
+ten: int = int(input('Ingresa un número: '))
+if ten*ten % 10 == 0:
+    print('Es multiplo')
+else:
+    print('No es multiplo')
