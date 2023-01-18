65efbe08ef83612abd3f2a778a91851f616d122d
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-01-18 14:25:57 -0600

    Se agrego el ejercicio 7-5

--- PythonExercices/Bucle_WHILE/TryitYourself_127.py
+++ PythonExercices/Bucle_WHILE/TryitYourself_127.py
@@ -15,3 +15,20 @@ while activeFlag:
         print('El ingrediente que agregaste es: ' + ingredients)
 
 
+# EJERCICIO 7-5 Movie tickets
+
+AgeUser: int = int(input('¿Cual es tu edad?: '))
+
+if AgeUser < 3:
+    print('Tu entrada es gratis.')
+elif AgeUser > 3 | AgeUser < 12:
+    print('Tu entrada cuesta $ 10.00')
+elif AgeUser > 12:
+    print('Tu entrada cuesta $ 15.00')
+
+
+
+
+
+
+
