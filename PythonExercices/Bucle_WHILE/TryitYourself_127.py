234f9aab1539da21dad6a2f4cfac23a8f23dd59b
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-01-19 18:23:45 -0600

    Correcciones de errores en el ejercicio 7-5, se tenia un BUG en la condicÃión de los rangos de edad de 4 a 11 años

--- PythonExercices/Bucle_WHILE/TryitYourself_127.py
+++ PythonExercices/Bucle_WHILE/TryitYourself_127.py
@@ -28,15 +28,16 @@ AgeMessage: str = "" # Mensaje vació que debe estar antes de comenzar el While.
 while AsientosDisponibles != 0:
     AgeMessage = int(input(AgeInstructions)) # Se hizo un cast para convertir de str a int.
     AsientosDisponibles -= 1 # Se va reduciendo en 1 cada vez que se ocupe in asiento
+    print ('* Asientos disponibles: ' + str(AsientosDisponibles))
 
     if AgeMessage == 999: # También si queremos que el programa se termine antes usamos break
-        print('Hasta la vista baby!')
+        print('\n\t¡Hasta la vista baby!')
         break
 
     elif AgeMessage <= 3:
         print('Tu entrada es gratis.')
 
-    elif AgeMessage > 3 | AgeMessage < 12:
+    elif AgeMessage in range(4,12):
         print('Tu entrada cuesta $ 10.00')
 
     elif AgeMessage >= 12:
