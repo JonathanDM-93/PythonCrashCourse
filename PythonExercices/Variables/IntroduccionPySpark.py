ff7e4e2e41a41f495d43122db5ff621fab4f37e5
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-02-03 12:33:50 -0600

    ItroducciÂón a PySpark

---
+++ PythonExercices/Variables/IntroduccionPySpark.py
@@ -0,0 +1,29 @@
+# Crear Sesion de spark
+from pyspark.sql import SparkSession
+spark =SparkSession.builder.appName('IntroduccionPySpark').getOrCreate()
+
+# Se necesita el SQLContext para programar DATAFRAMES
+from pyspark.sql import SQLContext
+# Cree un SQLContext a pasando como arguemnto a la sessión
+sql_context = SQLContext(spark)
+
+
+# cargar el archivo .csv
+df = spark.read.load("C:/Users/joni_/Downloads/peliculas.csv",
+                     format="csv", sep=",",
+                     inferSchema="true",
+                     header="true")
+
+# Crear un esquema en Python
+
+
+
+
+
+
+df.show()
+
+
+
+
+
