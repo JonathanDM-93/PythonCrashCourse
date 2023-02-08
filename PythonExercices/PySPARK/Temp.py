221b9fc4172d615698d46e9807d448d74dde6b58
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-02-08 12:38:40 -0600

    ModificacÃíon del archivo para intentar una conexión a la DB de redshift

--- PythonExercices/PySPARK/Temp.py
+++ PythonExercices/PySPARK/Temp.py
@@ -1,9 +1,12 @@
 from pyspark.sql import SQLContext
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 import boto3
 
 spark = SparkContext(master="local", appName="Temp").getOrCreate()
 sqlContext = SQLContext(spark)
+
+conf = SparkConf().setAppName("test").setMaster("local")
+sc = SparkContext(conf=conf)
 
 secretsmanager_client = boto3.client('secretsmanager')
 secret_manager_response = secretsmanager_client.get_secret_value(
@@ -11,8 +14,8 @@ secret_manager_response = secretsmanager_client.get_secret_value(
     VersionId='string',
     VersionStage='string'
 )
-username = "Mundo"
-password = "alondra"
+username = "mycluster"
+password = "Zamusmetroid10#"
 url = "jdbc:redshift://redshifthost:5439/database?user=" + str(username) + "&password=" + str(password)
 
 # Read data from a table
@@ -20,7 +23,7 @@ df = sqlContext.read \
     .format("io.github.spark_redshift_community.spark.redshift") \
     .option("url", "jdbc:redshift://mycluster.cfvkjcuj27ud.us-east-1.redshift.amazonaws.com:5439/dev") \
     .option("dev", "sales") \
-    .option("tempdir", "s3://jonathancubeta/tickitdb") \
+    .option("tempdir", "s3a://jonathancubeta/tickitdb") \
     .load()
 
 df.show()
--- PythonExercices/PySPARK/Test_connection.py
+++ PythonExercices/PySPARK/Test_connection.py
@@ -6,27 +6,11 @@ spark = SparkContext(master="local", appName="Test_connection")
 sqlContext = SQLContext(spark)
 
 # Read data from a table
-"""df = sqlContext.read \
+DataFrame_Sales = sqlContext.read \
     .format("io.github.spark_redshift_community.spark.redshift") \
     .option("url", "jdbc:redshift://mycluster.cfvkjcuj27ud.us-east-1.redshift.amazonaws.com:5439/dev") \
-    .option("dbtable", "sales") \
-    .option("tempdir", "s3n://jonathancubeta/tickitdb") \
-    .option("aws_iam_role", "arn:aws:iam::869215270926:role/Jonathan_Rol") \
-    .load()"""
-
-# Read data from a query
-"""df = sqlContext.read \
-    .option("url", "jdbc:redshift://mycluster.cfvkjcuj27ud.us-east-1.redshift.amazonaws.com:5439/dev") \
-    .option("query", "select x, count(*) sales") \
-    .option("tempdir", "s3n://jonathancubeta/tickitdb") \
-    .option("aws_iam_role", "arn:aws:iam::869215270926:role/Jonathan_Rol") \
-    .load()"""
-
-# Read data from a table
-df = sqlContext.read\
-    .option("url", "jdbc:redshift://mycluster.cfvkjcuj27ud.us-east-1.redshift.amazonaws.com:5439/dev") \
-    .option("dev", "sales") \
+    .option("dbtable", "tickitdb.sales") \
     .option("tempdir", "s3://jonathancubeta/tickitdb") \
     .load()
 
-df.show()
+DataFrame_Sales.show()
