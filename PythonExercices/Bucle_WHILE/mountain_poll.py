6572eced0b646f53866208c90925a03f233511a1
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-04-09 19:36:22 -0600

    Se realizo el ejemplo de agreagar valores a un diccionario, con el condicional while para que se termine la entrada de valores dependiedo una respuesta

--- PythonExercices/Bucle_WHILE/mountain_poll.py
+++ PythonExercices/Bucle_WHILE/mountain_poll.py
@@ -1,5 +1,33 @@
 # Ingresando valores a un diccionario
 # Crear una variable de in diccionario vació
-respuestas : dict = {}
+responses : dict = {}
 
 # Agrega una bandera
+polling_active : bool = True
+
+while polling_active:
+    # Pregunta al usuario su nombre
+    name: str = input ('¿Cuál es tu nombre? ')
+    response : str = input ('¿Qué montaña deseas escalar hoy? ')
+
+    # Guarda la respuesta en el diccionario
+    responses[name] = response
+
+    # Averigua si alguien mas quiere agregarse a la lista
+    repeat : str = input('¿Deseas agregar otra persona? (yes/no) ')
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results
+print ("\n --- Resultados ---")
+for name, response in responses.items():
+    print(name + " la montaña que deseas escalar es " + response)
+
+# --- Resultados ---
+# Jonathan la montaña que deseas escalar es Orizaba
+# Juan la montaña que deseas escalar es Perote
+
+
+
+
+
