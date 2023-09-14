3ab0cb78596cd2a9e2c52f92cd69a498638bc8e9
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-09-14 16:50:13 -0600

    Se hicieron cambios en el ejecicio Movie Tickets 7-5

--- PythonExercices/Bucle_WHILE/TryitYourself_127.py
+++ PythonExercices/Bucle_WHILE/TryitYourself_127.py
@@ -21,14 +21,14 @@ while activeFlag:
 AgeInstructions: str = "\nPresiona 999 para salir."
 AgeInstructions += '\n¿Cual es tu edad: '
 
-AsientosDisponibles: int = 5 # Variable que indica cuantos asientos están disponibles.
-AgeMessage: str = "" # Mensaje vació que debe estar antes de comenzar el While.
+AsientosDisponibles: int = 5  # Variable que indica cuantos asientos están disponibles.
+AgeMessage: str = ""  # Mensaje vació que debe estar antes de comenzar el While.
 
-#Crear el While con la condición que se terminara cuando el conteo sea igual a 0
+# Crear el While con la condición que se terminara cuando el conteo sea igual a 0
 while AsientosDisponibles != 0:
-    AgeMessage = input(AgeInstructions) # Se hizo un cast para convertir de str a int.
-    Age_two : int = int(AgeMessage) # Se casteo nuevamente en otra variable para que no saliera error.
-    AsientosDisponibles -= 1 # Se va reduciendo en 1 cada vez que se ocupe in asiento
+    AgeMessage = input(AgeInstructions)  # Se hizo un cast para convertir de str a int.
+    Age_two : int = int(AgeMessage)  # Se casteo nuevamente en otra variable para que no saliera error.
+    # AsientosDisponibles -= 1 # Se va reduciendo en 1 cada vez que se ocupe in asiento
     print ('* Asientos disponibles: ' + str(AsientosDisponibles))
 
     if Age_two == 999: # También si queremos que el programa se termine antes usamos break
@@ -37,12 +37,15 @@ while AsientosDisponibles != 0:
 
     elif Age_two <= 3:
         print('Tu entrada es gratis.')
+        AsientosDisponibles -= 1 # Se va reduciendo en 1 cada vez que se ocupe in asiento
 
-    elif Age_two in range(4,11):
+    elif Age_two in range(4, 11):
         print('Tu entrada cuesta $ 10.00')
+        AsientosDisponibles -= 1  # Se va reduciendo en 1 cada vez que se ocupe in asiento
 
     elif Age_two >= 12:
         print('Tu entrada cuesta $ 15.00')
+        AsientosDisponibles -= 1  # Se va reduciendo en 1 cada vez que se ocupe in asiento
 
 
 
