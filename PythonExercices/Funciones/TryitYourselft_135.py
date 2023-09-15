c0bb817e0ec5a268307e6039e607f59504f2eae1
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-09-14 18:26:54 -0600

    Finalización Try it Yourself pag135 ejecicio 8-1

---
+++ PythonExercices/Funciones/TryitYourselft_135.py
@@ -0,0 +1,9 @@
+# TRY IT YOURSELF pag. 135 List
+# Escribir una función basic que imprima un mensaje
+
+def message():
+    print("Declaración de funciones!")
+
+
+if __name__ == "__main__":
+    message()
