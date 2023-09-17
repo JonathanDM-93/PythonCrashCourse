a19453cb7d9cfe4c2ba04d4d212543e4c1848e47
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-09-16 22:06:14 -0600

    Se crearon algunas funciones para desarrollar nuevos aplicativos usandolas

--- PythonExercices/Funciones/TryitYourselft_135.py
+++ PythonExercices/Funciones/TryitYourselft_135.py
@@ -1,9 +1,22 @@
-# TRY IT YOURSELF pag. 135 List
+# TRY IT YOURSELF pag. 135 8-1 Message
 # Escribir una función basic que imprima un mensaje
 
 def message():
     print("Declaración de funciones!")
 
 
+# TRY IT YOURSELF pag. 135 8-2 Favorite Book
+# Declarar la función aquí
+def book(libro):
+    print(f"Este es tu libro favorito: {libro} ")
+
+
+# Ejecución del script
 if __name__ == "__main__":
+
+    # LLamar a la función message
     message()
+
+    # Llamar a la función book
+    favorite_book = str(input("¿Cuál es tu libro favorito?"))
+    book(favorite_book)
