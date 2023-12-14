11a4e9cccbd544bde3ff7ade016e830cbd0a5fba
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2023-12-14 13:55:15 -0600

    Se tomo el ejemplo de la clase Car() para crear un clase hija, se puede apreciar en el código el tema de HERENCIA

---
+++ PythonExercices/ Classes/electric_car.py
@@ -0,0 +1,44 @@
+class Car:
+    """Initialize attributes to describe a car"""
+
+    def __init__(self, make: str, model: str, year: str):
+        self.make = make
+        self.model = model
+        self.year = year
+        """Setting a Default Value for an Attribute"""
+        self.odometer_reading = 0
+
+    def get_describe_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + str(self.make) + ' ' + str(self.model)
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car´s mileage"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value"""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can´t roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading"""
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    def __init__(self, make: str, model: str, year: str):
+        """Connection between parent class and child class"""
+        super().__init__(make, model, year)
+
+
+# ----------------------------------------------------------- #
+
+# Crear una instancia/objeto
+myToyota = ElectricCar("Toyota", "Yaris", "2023")
+
+# Llamar al método/función de la parent class
+print(myToyota.get_describe_name())
