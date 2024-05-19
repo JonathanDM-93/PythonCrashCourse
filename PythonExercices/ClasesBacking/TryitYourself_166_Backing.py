3d5c5e1155bb6198e6fc257c3518af1e33fef857
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2024-05-19 16:57:16 -0600

    Se creo un nuevo ejercicio y vemos que podemos hacer con las clases y agregar tantos atrubutos como necesitemos, y llamarlos a traves de los difrentes métodos y ponernos creativos. Esto se pone emocinante espero hacer ya clases con los ojos vendados

--- PythonExercices/ClasesBacking/TryitYourself_166_Backing.py
+++ PythonExercices/ClasesBacking/TryitYourself_166_Backing.py
@@ -30,7 +30,6 @@ someRestaurant.open_restaurant()
 
 print("\n[*]\n")
 
-
 # TRY IT YOURSELF pag. 166 9-2 Three Restaurants
 # Toma la clase anterior y crea tres diferentes instancias/objetos a partir de la clase y llama a describe_restaurant() para
 # cada instancia.
@@ -44,9 +43,42 @@ print("\n[-]\n")
 japanRestaurant = Restaurant("El palillito feliz", "Japonea")
 japanRestaurant.describe_restaurant()
 
-
 print("\n[*]\n")
 
 
+# TRY IT YOURSELF pag. 166 9-3 Users
+# 1.- Crea una clase llamada User.
+# 2.- Crea dos atributos first_name & last_name, y otros atributos que consideres que son típicamente usados al crear
+# el perfil de un usuario
+# 3.- Crea un método llamado describe_user() que imprima la información del usuario.
+# 4.- Crea un segundo método llamado greet_user() que imprima un saludo personalizado para el usuario.
 
 
+class User:
+    """Initialize attributes"""
+
+    def __init__(self, first_name: str, last_name: str, address: str, telephone: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.telephone = telephone
+
+    def greet_user(self):
+        print(f"Mucho gusto: {self.first_name.title()} {self.last_name.title()} !")
+
+    def describe_user(self):
+        print(f"Esta es tu información:\n"
+              f" [*] Nombre: {self.first_name.title()} \n"
+              f" [*] Apellido: {self.last_name.title()} \n"
+              f" [*] Dirección: {self.address.title()} \n"
+              f" [*] Telefono: {self.telephone.title()}")
+
+
+# Crear una instancia/objeto a partir de la clase
+oneUser = User("Thomas", "Mackensee", "Lawrenceville, Georgia", "+441 362372888")
+
+# Llamar al método/función greet_user() para mandar un saludo de bienvenida al usuario.
+oneUser.greet_user()
+
+# Llamar al método/función describe_user() la cual imprime la data del usuario.
+oneUser.describe_user()
