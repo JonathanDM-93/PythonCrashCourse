763cb847bdc938e9d6cb9d8a2c0d989b09762993
Jonathan de la Rosa Maldonado <jhon_delarosam93@outlook.es>
2024-05-19 16:33:35 -0600

    Se creo el la segunda parte del ejercicio de repaso y solo creamos tres instancias diferentes que tomas los atributos que se les asignaron y llama al método describe para imprimir en pantalla un mensaje sobre el restaurante en cuestión

--- PythonExercices/ClasesBacking/TryitYourself_166_Backing.py
+++ PythonExercices/ClasesBacking/TryitYourself_166_Backing.py
@@ -21,11 +21,32 @@ class Restaurant:
 # Crear una instancia/objeto de la clase Restaurant()
 someRestaurant = Restaurant("El italiano loco", "Italiana")
 
-# someRestaurant.describe_restaurant()
-# someRestaurant.open_restaurant()
+someRestaurant.describe_restaurant()
+someRestaurant.open_restaurant()
 
 # El nombre del restaurante es: El Italiano Loco
 # El tipo de cocina es: Italiana
-# El restaurante: El Italiano Loco esta abierto!
+# El restaurante: El Italiano Loco está abierto!
+
+print("\n[*]\n")
+
+
+# TRY IT YOURSELF pag. 166 9-2 Three Restaurants
+# Toma la clase anterior y crea tres diferentes instancias/objetos a partir de la clase y llama a describe_restaurant() para
+# cada instancia.
+
+mexicanRestaurant = Restaurant("El Mexicanito", "Mexicana")
+mexicanRestaurant.describe_restaurant()
+print("\n[-]\n")
+ruskyRestaurant = Restaurant("El rusky bailarin", "Rusa")
+ruskyRestaurant.describe_restaurant()
+print("\n[-]\n")
+japanRestaurant = Restaurant("El palillito feliz", "Japonea")
+japanRestaurant.describe_restaurant()
+
+
+print("\n[*]\n")
+
+
 
 
